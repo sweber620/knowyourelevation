23cdef4c214f633ba04dc9b439391b09f0e2c981
merp101 <34143165+merp101@users.noreply.github.com>
2018-10-26 11:15:41 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -3,6 +3,6 @@ var elevation = 0;
 setInterval(
   function() {
     document.getElementById("timer").innerHTML =elevation + " feet";
-    elevation += (39000 / 60) / 60;
+    elevation += ((39000 / 60) / 60) / 1000;
   }, 
 1);
