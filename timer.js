f09152ffc1608ef57d996c6f1b885ce3630c2676
merp101 <34143165+merp101@users.noreply.github.com>
2018-10-26 11:27:08 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -6,6 +6,7 @@ setInterval(
       document.getElementById("timer").innerHTML =elevation.toFixed(1) + " feet";
       elevation += ((39000 / 60) / 60) / 250;
     } else {
-      document.getElementById("timer").innerHTML = "You've passed out..."
+      document.getElementById("timer").innerHTML = "You've passed out...";
+    }
   }, 
 1);
