938accaa1bdf991866f0022f44c5f205fb4f2704
merp101 <34143165+merp101@users.noreply.github.com>
2018-10-26 11:36:14 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -2,11 +2,13 @@ var elevation = 0;
 
 setInterval(
   function() {
-    if (elevation < 39000) {
+    if (elevation >= 39000) {
+      document.getElementById("timer").innerHTML = "You've passed out...";
+    } else  if (elevation >= 10000) {
+      //giveHint(); 
+    } else {
       document.getElementById("timer").innerHTML =elevation.toFixed(1) + " feet";
       elevation += ((39000 / 60) / 60) / 250;
-    } else {
-      document.getElementById("timer").innerHTML = "You've passed out...";
     }
   }, 
 1);
