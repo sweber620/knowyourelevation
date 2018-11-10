2880a6ad3251dd38b7a8611e9dd820afdfeedaa6
merp101 <34143165+merp101@users.noreply.github.com>
2018-11-09 21:57:05 -0500

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,4 +1,5 @@
 var elevation = 0;
+var thingy;
 var sec = 0;
 var min = 60;
 
@@ -9,14 +10,19 @@ setInterval(
     } else  if (elevation >= 10000) {
       //giveHint(); 
     } else {
+      thingy += 1;
+      if (thingy = 1000) {
+          if (sec == 1) {
+          sec = 60;
+          min -= 1;
+        } else sec -= 1;
+        document.getElementById("TtimerS").innerHTML = sec;
+       document.getElementById("TtimerM").innerHTML = min;
+        thingy = 0;
+      }
       document.getElementById("Etimer").innerHTML =elevation.toFixed(1) + " feet";
       elevation += ((39000 / 60) / 60) / 250;
-      if (sec == 1) {
-        sec = 60;
-        min -= 1;
-      } else sec -= 1;
-      document.getElementById("TtimerS").innerHTML = sec;
-      document.getElementById("TtimerM").innerHTML = min;
+      
     }
   }, 
 1);
