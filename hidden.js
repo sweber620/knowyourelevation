9bbd942c570e57f924053011867f61fa41bba556
merp101 <34143165+merp101@users.noreply.github.com>
2018-11-10 07:28:46 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -1,1 +1,10 @@
-
+var placeholder;
+function input(num) {
+  switch(num) {
+    case 1: if(document.getElementById("capitalLocation").value == "Helsinki") {showRiddle();}
+  }
+}
+function showRiddle() {
+  document.getElementById("topLeftStuff").display="none";
+}
+  
