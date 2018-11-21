6e1642d37b4b62985d0261d911a767982af8f1b0
merp101 <34143165+merp101@users.noreply.github.com>
2018-11-20 19:41:29 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -1,15 +1,14 @@
 var placeholder;
 var cheatervar = 0;
-function input(num) {
+function showRiddle() {
+  document.getElementById("topLeftStuff").display="none";
+}
+  
+window.addEventListener('keyup', function(num=1) {
   if (event.keyCode == 13) { //enter
     switch(num) {
       case 1: if(document.getElementById("capitalLocation").value == "Helsinki") {showRiddle();}
     }
   } else if (event.keyCode == 123 || event.keyCode == 17 || event.keyCode == 122) {alert("No cheating! Next time, you'll automatically lose!"); cheatervar+=1;}
   if (cheatervar == 2) {elevation = 39000;}
-}
-function showRiddle() {
-  document.getElementById("topLeftStuff").display="none";
-}
-  
-window.addEventListener('keyup', input(1));
+});
