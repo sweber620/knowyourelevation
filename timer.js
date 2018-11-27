7917dc876b2bfcfc34fd280d15609a9155a0d0c0
merp101 <34143165+merp101@users.noreply.github.com>
2018-11-27 14:43:34 -0500

    Update timer.js

--- timer.js
+++ timer.js
@@ -3,26 +3,26 @@ var ms = 1;
 var sec = 60;
 var min = 39;
 
-document.getElementById("body").onload = function() {
-  setInterval(
-    function() {
-      if (elevation >= 39000) {
-        document.getElementById("body").style.backgroundColor="black";
-        document.getElementById("container").display="none";
-      } else {
-        ms += 1;
-        if (ms == 250) {
-           if (sec == 1) {
-            sec = 60;
-            min -= 1;
-           } else sec -= 1;
-           document.getElementById("TtimerS").innerHTML = sec;
-           document.getElementById("TtimerM").innerHTML = min;
-           ms = 1;
-        }
-      document.getElementById("Etimer").innerHTML = elevation.toFixed(1) + " feet";
-      elevation += (((39000 / 60) / 40) / 250) * mult;
-      }
-    }, 
-  1);
-}
+
+setInterval(
+  function() {
+    if (elevation >= 39000) {
+      document.getElementById("body").style.backgroundColor="black";
+      document.getElementById("container").display="none";
+    } else {
+      ms += 1;
+      if (ms == 250) {
+         if (sec == 1) {
+           sec = 59;
+           min -= 1;
+          } else sec -= 1;
+          document.getElementById("TtimerS").innerHTML = sec;
+          document.getElementById("TtimerM").innerHTML = min;
+          ms = 1;
+       }
+     document.getElementById("Etimer").innerHTML = elevation.toFixed(1) + " feet";
+     elevation += (13/200) * mult;
+     }
+   }, 
+1);
+
