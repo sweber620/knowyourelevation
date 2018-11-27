d704f37e203b8801d5281236456c7cbcafeeb8f0
merp101 <34143165+merp101@users.noreply.github.com>
2018-11-27 14:36:38 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -1,15 +1,13 @@
 var cheater = false;
 var mult = 1;
-document.getElementById("body").onload = function() {
-  body.addEventListener('keypress', function() {
-    var x = event.keyCode;
-    if (event.keyCode == 13) { //enter
-        if (document.getElementById("capitalLocation").value == "Helsinki") {}
-    } else if (x == 123 || x == 17 || x == 122 || x == 116 || x == 112 || x == 113 || x == 114 || x == 115) { // f1, f2, f3, f4, f5, f11, f12
-      if (!cheater) {
-        alert("No cheating! Next time, you'll automatically lose!"); 
-        cheater = true;
-      } else if (cheater) {mult = 1000}
-    }
-  });
-}
+body.addEventListener('keypress', function() {
+   var x = event.keyCode;
+   if (event.keyCode == 13) { //enter
+       if (document.getElementById("capitalLocation").value == "Helsinki") {}
+   } else if (x == 123 || x == 17 || x == 122 || x == 116 || x == 112 || x == 113 || x == 114 || x == 115) { // f1, f2, f3, f4, f5, f11, f12
+     if (!cheater) {
+       alert("No cheating! Next time, you'll automatically lose!"); 
+       cheater = true;
+     } else if (cheater) {mult = 1000}
+   }
+});
