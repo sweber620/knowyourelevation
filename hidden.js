0c3975d095cfc08dffc357240dc42a1042ee12c5
merp101 <34143165+merp101@users.noreply.github.com>
2018-11-27 14:32:41 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -6,9 +6,10 @@ function showRiddle() {
 
 document.getElementById("body").onload = function() {
   body.addEventListener('keypress', function() {
+    var x = event.keyCode;
     if (event.keyCode == 13) { //enter
         if (document.getElementById("capitalLocation").value == "Helsinki") {showRiddle();}
-    } else if (event.keyCode == 123 || event.keyCode == 17 || event.keyCode == 122) {
+    } else if (x == 123 || x == 17 || x == 122 || x == 116 || x == 112 || x == 113 || x == 114 || x == 115) { // f1, f2, f3, f4, f5, f11, f12
       if (!cheater) {
         alert("No cheating! Next time, you'll automatically lose!"); 
         cheater = true;
