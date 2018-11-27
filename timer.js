77bacb3d7229ff71ee9d717831ad95eeafda8582
merp101 <34143165+merp101@users.noreply.github.com>
2018-11-27 14:26:40 -0500

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,27 +1,28 @@
 var elevation = 0;
-var thingy = 1;
+var ms = 1;
 var sec = 60;
 var min = 39;
 
-setInterval(
-  function() {
-    if (elevation >= 39000) {
-      document.getElementById("body").style.backgroundColor="black";
-      document.getElementById("container").display="none";
-    } else {
-       thingy += 1;
-       if (thingy == 250) {
+document.getElementById("body").onload = function() {
+  setInterval(
+    function() {
+      if (elevation >= 39000) {
+        document.getElementById("body").style.backgroundColor="black";
+        document.getElementById("container").display="none";
+      } else {
+        ms += 1;
+        if (ms == 250) {
            if (sec == 1) {
             sec = 60;
             min -= 1;
            } else sec -= 1;
-         document.getElementById("TtimerS").innerHTML = sec;
-         document.getElementById("TtimerM").innerHTML = min;
-         thingy = 1;
-       }
-      document.getElementById("Etimer").innerHTML =elevation.toFixed(1) + " feet";
+           document.getElementById("TtimerS").innerHTML = sec;
+           document.getElementById("TtimerM").innerHTML = min;
+           ms = 1;
+        }
+      document.getElementById("Etimer").innerHTML = elevation.toFixed(1) + " feet";
       elevation += (((39000 / 60) / 40) / 250) * mult;
-      
-    }
-  }, 
-1);
+      }
+    }, 
+  1);
+}
