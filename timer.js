6beedc70fb04a5da792552d00b87854a6ac4f700
merp101 <34143165+merp101@users.noreply.github.com>
2018-12-06 13:24:09 -0500

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,26 +1,7 @@
-var elevation = 0;
+var elevation = 39000;
 var ms = 1;
 var sec = 60;
 var min = 39;
-
-function simulateTime(seconds, real) {
-
-    //the game is simulated at a 50ms update rate, with a max of 1000 ticks
-    //warning: do not call this function with real unless you know what you're doing
-    document.getElementById("offlineprogress").style.display = "block"
-    var ticks = seconds * 20;
-    var bonusDiff = 0;
-    var playerStart = Object.assign({}, player);
-    if (ticks > 1000 && !real) {
-        bonusDiff = (ticks - 1000) / 20;
-        ticks = 1000;
-    }
-    let ticksDone = 0
-    for (ticksDone=0; ticksDone<ticks; ticksDone++) {
-        loop();
-        if (real) console.log(ticksDone)
-    }
-}
 
 function loop() {
   if (elevation >= 39000) {
@@ -38,7 +19,7 @@ function loop() {
           ms = 1;
        }
      document.getElementById("Etimer").innerHTML = elevation.toFixed(1) + " feet";
-     elevation += (13/200) * mult;
+     elevation -= (13/200) * mult;
      }
 }
 
