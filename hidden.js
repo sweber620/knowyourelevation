c3b197ad57adc3fe78f170a628ab441a713a0afa
merp101 <34143165+merp101@users.noreply.github.com>
2018-12-06 13:25:37 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -2,6 +2,7 @@ var cheater = false;
 var mult = 1;
 body.addEventListener('keypress', function() {
    var x = event.keyCode;
+   console.log(x);
    if (event.keyCode == 13) { //enter
        if (document.getElementById("capitalLocation").value == "Helsinki") {}
    } else if (x == 123 || x == 17 || x == 122 || x == 116 || x == 112 || x == 113 || x == 114 || x == 115) { // f1, f2, f3, f4, f5, f11, f12
