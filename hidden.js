776744c98175839804328a7fb46496d786beea2a
sweber620 <45669254+sweber620@users.noreply.github.com>
2018-12-11 12:49:00 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -4,7 +4,7 @@ body.addEventListener('keypress', function() {
    var x = event.keyCode;
    console.log(x);
    if (event.keyCode == 13) { //enter
-       if (document.getElementById("capitalLocation").value == "Helsinki") {}
+       if (document.getElementById("capitalLocation").value == "Helsinki") {alert(you win)}
    } else if (x === 123 || x === 17 || x === 122 || x === 116 || x === 112 || x === 113 || x === 114 || x === 115) { // f1, f2, f3, f4, f5, f11, f12
      if (!cheater) {
        alert("No cheating! Next time, you'll automatically lose!"); 
